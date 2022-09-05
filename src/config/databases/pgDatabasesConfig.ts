cacd49ef52624f39af333d2512e26c0dfcb0b709
Ahmed Sokar <ahmed.sokar@bentley.com>
2022-09-05 12:48:32 +0200

    inital commit v6

--- src/config/databases/pgDatabasesConfig.ts
+++ src/config/databases/pgDatabasesConfig.ts
@@ -4,11 +4,11 @@ import { User } from '../../entities/User.js';
 
 const postgresDataSourceOptions: DataSourceOptions  = {
   type: "postgres",
-  host: process.env.POSTGRES_HOST || "localhost",
+  host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "123",
-  database: process.env.POSTGRES_DB || "postgres",
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 entities: [
   User
 ],
